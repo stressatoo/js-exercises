f121962088e8b0f8786c2875081d22c572f8c6d9
Alessandro <tryit0utspectre@gmail.com>
2022-12-25 12:11:11 +0100

    Update flow

--- rockpaperscissors/index.js
+++ rockpaperscissors/index.js
@@ -4,6 +4,27 @@ function getComputerChoice() {
       return choices[Math.floor(Math.random()*choices.length)];
 }
 
+function playRound(playerSelection, computerSelection) {
+      let playerSelection = (prompt("Insert choice (Rock / Paper / Scissors)")).toLowerCase();
+
+      let result;
+
+      return result;
+}
+
+function game() {
+      for (let i = 1; i <= 5; ++i) {
+            playRound();
+      }
+}
+
 getComputerChoice();
 
-console.log(getComputerChoice()); // works
+let computerSelection = getComputerChoice();
+
+
+/*
+Write a function that plays a single round of Rock Paper Scissors. 
+The function should take two parameters - the playerSelection and computerSelection - 
+and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+*/
